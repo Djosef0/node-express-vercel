d32948e340b9234ddadfb5080ae4891afdc01625
jayakrishnamurthy <youssefbensaid827@gmail.com>
2022-10-04 22:28:40 +0200

    first commit

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const clotherRoute = require("./routes/clothers");
 const listRoute = require("./routes/lists");
+const path = require("path");
+const cors = require("cors");
 
 
 dotenv.config();
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -27,6 +30,17 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/clothers", clotherRoute);
 app.use("/api/lists", listRoute);
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get('*', function(_, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html')),
+  function (err){
+    if(err){
+      res.status(500).send(err);
+    }
+  }
+});
 
 app.listen(process.env.PORT || 8800, () => {
   console.log("Backend server is running!");
